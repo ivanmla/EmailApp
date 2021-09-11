f2eb9bf37e4ce9831860c5f56c96f3ea92f1db08
ivanmla <ivan0mlakic@gmail.com>
2021-09-11 13:02:00 +0200

    inicijalni client

--- API/Startup.cs
+++ API/Startup.cs
@@ -43,6 +43,8 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("SqliteConnection"));
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +60,10 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
